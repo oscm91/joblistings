c2cdc1e673286a8531acafa983cb5b5fd23e7090
oscarmora <oscm91@hotmail.com>
2023-07-12 07:23:54 -0500

    Deploy config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LanguageComponent } from './components/language/language.component';
@@ -9,7 +8,7 @@ import { TagContainerComponent } from './components/tag-container/tag-container.
 
 @NgModule({
   declarations: [AppComponent, LanguageComponent, TagContainerComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
